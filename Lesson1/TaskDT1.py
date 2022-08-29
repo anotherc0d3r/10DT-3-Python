919c2e12ddfbd7caf08e2558a83c97b97be7e15f
anotherc0d3r <103463225+anotherc0d3r@users.noreply.github.com>
2022-08-30 09:03:35 +1200

    Add files via upload

---
+++ Lesson1/TaskDT1.py
@@ -0,0 +1,22 @@
+# F B Cowan
+# Practice Task
+
+# variables (draft)
+# Height is in Cm
+Name="Fletcher"
+Age = 14
+Height = 165 
+Address="26 Tukanae St, Wellington"
+number = "0224722135"
+Allergy = False
+
+# A summary of this information should be printed out to the screen
+
+print ('My name is', Name)
+print ('My age is', Age)
+print ('My height is', Height, 'Cm')
+print ('My address is', Address)
+print ('My phone number is', number)
+print ('Allergy:', Allergy)
+
+# clarifying the problem
