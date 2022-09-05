4cf9622cdb42401237e0e65bc0ba4c29fc2b0408
anotherc0d3r <103463225+anotherc0d3r@users.noreply.github.com>
2022-09-06 09:48:40 +1200

    Add files via upload

---
+++ Lessom2/input_user.py
@@ -0,0 +1,32 @@
+# F B Cowan
+# Input - various types by user
+
+# input() function
+
+# Taking input from the user
+
+
+# output
+
+
+name = input("Enter your name:")
+print("Hello, ", name)
+
+# Taking input from the user as integer
+# num = int(input("Enter a number: "))
+# add = num + 1
+# output
+# print(add)
+
+
+# Taking input from the user as integer
+# num = float(input("Enter a number: "))
+# add = num + 1
+# output
+# print(add)
+
+
+# Taking input from the user as integer
+# li = list(input("Enter a number: "))
+# output
+# print(li)
