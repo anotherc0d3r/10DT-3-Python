eab2720b30090668c550b739c3c5a180641b29bd
anotherc0d3r <103463225+anotherc0d3r@users.noreply.github.com>
2022-09-07 11:04:56 +1200

    Add files via upload

---
+++ Lessom2/Problem 2.py
@@ -0,0 +1,14 @@
+# F B Cowan
+# representing number data types as string
+
+# Problem 02
+# This program is designed to track the value on a gift card as it is used.
+
+
+Card_Balance = 50.00
+Purchase_01 = 35.00
+print ("Your current balance: $" + str (Card_Balance))
+
+Card_Balance = Card_Balance - Purchase_01
+print ("Purchase_01: $" + str (Purchase_01))
+print ("Your New Balance is: $" + str (Card_Balance))
