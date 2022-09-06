328f9e7485cb4cfd92b68bae5fe04b93d09d1fa7
anotherc0d3r <103463225+anotherc0d3r@users.noreply.github.com>
2022-09-07 10:49:31 +1200

    Add files via upload

---
+++ Lessom2/Problem 1.py
@@ -0,0 +1,11 @@
+# F B Cowan
+# Calculations
+
+# problem 1
+# This program is designed to track and total goals scored by the scots 3rd XI for each game played.
+
+match1_ST_Pats = 7
+match2_Well_Col = 5
+match3_Well_High = 10
+Team_Total = match1_ST_Pats + match2_Well_Col + match3_Well_High
+print ("Total Goals Scored ", Team_Total)
