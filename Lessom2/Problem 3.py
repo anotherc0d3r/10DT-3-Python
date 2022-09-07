6821778e8576ce599eefcd754e83439f090a0197
anotherc0d3r <103463225+anotherc0d3r@users.noreply.github.com>
2022-09-07 20:41:15 +1200

    Add files via upload

---
+++ Lessom2/Problem 3.py
@@ -0,0 +1,17 @@
+# F B Cowan
+
+dieselprice = 1.47
+# Remember variable names connot begin with a digit
+petrol95price = 2.17
+petrol91price = 2.06
+print("Initial price:")
+print("Diesel price $" + str(dieselprice))
+print("95 price $" + str(petrol95price))
+print("91 unleaded price $" + str(petrol91price))
+dieselprice = dieselprice * 1.1
+petrol95price = petrol95price * 1.1
+petrol91price = petrol91price * 1.1
+print("Price increase of 10%:")
+print("Diesel price $" + str(dieselprice))
+print("95 price $" + str(petrol95price))
+print("91 unleaded price $" + str(petrol91price))
