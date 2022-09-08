5ccd060ba9536fb495eddb00ddcea5bfe176da4a
anotherc0d3r <103463225+anotherc0d3r@users.noreply.github.com>
2022-09-08 12:34:11 +1200

    Add files via upload

---
+++ Lessom2/variable print with dollar sign.py
@@ -0,0 +1,12 @@
+# F B Cowan
+dollars = 5
+print("$", dollars, ".00")
+dollars = 5
+print("$", end='')
+print(dollars, end='')
+print(".00")
+
+winner = "Water Boy"
+print("First place", winner)
+print("First place", end = ' ')
+print(winner)
